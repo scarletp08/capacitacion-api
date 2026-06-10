76c0294b8cff7eb2341ca462713634f3d46d806e
Scarlet Hiraldo <scarlet15hiraldo@gmail.com>
2026-06-10 11:24:15 -0400

    actualiza main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = FastAPI()
 
 # Conexión MongoDB Atlas
 client = MongoClient(
-    "mongodb+srv://prueba:ZA5DjJgHLMxLivDNK@cluster0.p2djwko.mongodb.net/?appName=Cluster0"
-   
+    "mongodb+srv://prueba:ZA5DjJgHLMxLivDNK@cluster0.p2djwko.mongodb.net/?retryWrites=true&w=majority"
+  
 )
 
 db = client["capacitacion"]
@@ -22,12 +22,19 @@ def inicio():
 
 @app.post("/formulario")
 def formulario(data: dict):
+    try:
+        resultado = collection.insert_one(data)
+        print("INSERT OK:", resultado.inserted_id)
 
-    resultado = collection.insert_one(data)
+        return {
+            "ok": True,
+            "id": str(resultado.inserted_id)
+        }
 
-    return {
-        "ok": True,
-        "id": str(resultado.inserted_id)
-    }
+    except Exception as e:
+        print("ERROR MONGO:", str(e))
 
-
+        return {
+            "ok": False,
+            "error": str(e)
+        }
