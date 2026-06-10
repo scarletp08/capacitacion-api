6a6102664039a19fd59f13c86576c2c6b9522da0
Scarlet Hiraldo <scarlet15hiraldo@gmail.com>
2026-06-10 17:42:24 -0400

    Dashbaord

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
 from pymongo import MongoClient
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 MONGO_URI = "mongodb+srv://prueba:tWdYsznMmn5yZ8BQ@cluster0.p2djwko.mongodb.net/?appName=Cluster0"
 
 
